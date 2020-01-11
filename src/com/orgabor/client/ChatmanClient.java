1cb99a5804bab4dc06ffcc5076e6d7ea2dc698de
Gabor <orbangabeszb@gmail.com>
2020-01-11 19:08:16 +0100

    client package added with ChatmanClient main class and fxml

--- src/com/orgabor/client/ChatmanClient.java
+++ src/com/orgabor/client/ChatmanClient.java
@@ -1,27 +1,30 @@
-package application;
-	
+package com.orgabor.client;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
-public class ChatmanServer extends Application {
+public class ChatmanClient extends Application {
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
 		try {
-			primaryStage.setTitle("Chatman Server");
-			Parent root = FXMLLoader.load(getClass().getResource("ChatmanServer.fxml"));
-			Scene scene = new Scene(root,300,400);
+			primaryStage.setTitle("Chatman");
+			Parent root = FXMLLoader.load(getClass().getResource("ChatmanClient.fxml"));
+			Scene scene = new Scene(root, 400, 300);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	
+
 }
