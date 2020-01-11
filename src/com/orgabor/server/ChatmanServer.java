88d1d96cfa82c4201e7cc3f0e35de0886d34c806
Gabor <orbangabeszb@gmail.com>
2020-01-11 19:31:50 +0100

    client gui outlined

--- src/com/orgabor/server/ChatmanServer.java
+++ src/com/orgabor/server/ChatmanServer.java
@@ -6,7 +6,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class ChatmanServer extends Application {
 	@Override
 	public void start(Stage primaryStage) {
