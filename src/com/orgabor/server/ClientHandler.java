4c6f0d1e48cae05d533141f7980159462ab59d65
Gabor <orbangabeszb@gmail.com>
2020-01-13 19:19:13 +0100

    ClientHandler uses ObjectInput/OutputStreams instead of reader/writer

--- src/com/orgabor/server/ClientHandler.java
+++ src/com/orgabor/server/ClientHandler.java
@@ -1,28 +1,22 @@
 package com.orgabor.server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
 	private Socket clientSocket;
-	private BufferedReader input;
-	private PrintWriter output;
+	private ObjectInputStream input;
+	private ObjectOutputStream output;
 	
 	private boolean isRunning;
-	
-	private String inputLine;
-	private String outputLine;
 	
 	public ClientHandler(Socket clientSocket) {
 		this.clientSocket = clientSocket;
 		try {
-			this.input = new BufferedReader(
-	   			   	     new InputStreamReader(
-		   				     clientSocket.getInputStream()));
-			this.output = new PrintWriter(clientSocket.getOutputStream(), true);
+			this.input = new ObjectInputStream(clientSocket.getInputStream());
+			this.output = new ObjectOutputStream(clientSocket.getOutputStream());
 		} catch(IOException e) {
 			e.printStackTrace();
 		}	
@@ -34,7 +28,7 @@ public class ClientHandler implements Runnable {
 
 		while(isRunning) {
 			try {
-				inputLine = input.readLine();
+				
 				
 				
 				
