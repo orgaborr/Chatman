62966b384ddeb1c19361c61b969a6a5b0b26e16a
Gabor <orbangabeszb@gmail.com>
2020-01-13 19:22:48 +0100

    created Message class outside or server=client packages

---
+++ src/com/orgabor/Message.java
@@ -0,0 +1,21 @@
+package com.orgabor;
+
+public class Message {
+	private String messageText;
+	private String date;
+	
+	public String getMessageText() {
+		return messageText;
+	}
+	public void setMessageText(String messageText) {
+		this.messageText = messageText;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	
+}
