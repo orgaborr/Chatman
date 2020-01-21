07250b25850b69389e4f64cf3770a70c571e0792
Gabor <orbangabeszb@gmail.com>
2020-01-21 16:51:28 +0100

    static serverController added to ChatmanServer

--- src/com/orgabor/server/ChatmanServer.java
+++ src/com/orgabor/server/ChatmanServer.java
@@ -7,14 +7,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class ChatmanServer extends Application {
+	static ServerController serverController;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(ServerController.class.getResource("ChatmanServer.fxml"));
+			FXMLLoader loader = new FXMLLoader(ServerController.class.getResource("ChatmanServer.fxml"));
+			Parent root = loader.load();
 			primaryStage.setTitle("Chatman Server");
 			primaryStage.setScene(new Scene(root,300,400));		
 			primaryStage.show();
 			
+			serverController = loader.getController();
 			primaryStage.setOnCloseRequest(e -> Server.getInstance().closeConnections());
 
 		} catch(Exception e) {
