76b517d7a34e8979c33a026ca425be6ac517f9fb
Gabor <orbangabeszb@gmail.com>
2020-02-19 13:02:44 +0100

    created LoginController and connected usernameField and loginButton

---
+++ src/com/orgabor/client/login/LoginController.java
@@ -0,0 +1,12 @@
+package com.orgabor.client.login;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+	@FXML
+	private TextField usernameField;
+	@FXML
+	private Button loginButton;
+}
